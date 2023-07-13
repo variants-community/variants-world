0ffa54dbe55548202ba51353e6c8a8612b1ddd91
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-07-13 23:19:46 +0300

    feat: add main deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import preact from "@astrojs/preact";
+
+import windi from "astro-windi";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [preact(), windi()]
+});

cb5f47e89fe8a1b7bcffe84ed988daac5bf762ed
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-07-31 18:22:59 +0300

    feat: sketch of Search on index page

---
+++ src/components/PostsList.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'preact/hooks'
+import PostCard from './PostCard'
+import Search from './Search'
+import type { PostWithDetailsForCard } from '../db/prisma/queries'
+
+// can it be stored in the db as field?
+const getTextForSearch = (post: PostWithDetailsForCard): string => {
+  const value = (post.title + post.author.name + post.description + post.type)
+    .toLowerCase()
+  console.log('rs: ', value)
+  return value
+}
+
+type PostsListProps = {
+  posts: PostWithDetailsForCard[];
+};
+
+const PostsList = (props: PostsListProps) => {
+  const [query, setQuery] = useState('')
+  const [filteredPosts, setFilteredPosts] = useState<PostWithDetailsForCard[]>(
+    props.posts,
+  )
+
+  useEffect(() => {
+    setFilteredPosts(
+      props.posts.filter((post) => getTextForSearch(post).includes(query.toLowerCase())),
+    )
+  }, [query])
+
+  return (
+    <>
+      <Search query={query} setQuery={setQuery} />
+
+      <div className="flex flex-col gap-[30px] mt-[55px]">
+        {filteredPosts.map((post) => <PostCard key={post.id} post={post} />)}
+      </div>
+    </>
+  )
+}
+
+export default PostsList
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,23 @@
+import { getValueFromEvent } from '../hepers'
 import SearchIcon from './icons/SearchIcon'
 
-const Search = () => {
+type SearhProps = {
+  query: string
+  setQuery: (query: string) => void
+}
+
+const Search = (props: SearhProps) => {
+
+  const onQueryInput = async (e: Event) => {
+    props.setQuery(getValueFromEvent<string>(e))
+  }
+
  return (
   <div
     className={'flex flex-row items-center w-full bg-dark shadow-light border border-[2px] border-border-dark rounded-full'}
   >
    <SearchIcon className='h-[20px] w-[20px] ml-[20px] my-[12px] mr-[12px]'/>
-   <input type="text" className={'w-full bg-dark outline-none text-[20px] font-normal'} placeholder={'Search'} />
+   <input value={props.query} onInput={onQueryInput} type="text" className={'w-full bg-dark outline-none text-[20px] font-normal'} placeholder={'Search'} />
    <div className={'flex flex-row mr-[20px] gap-[4px] text-[16px]'}>
     <span className={'py-[3px] px-[6px] bg-border-light rounded-[3px]'}>CTRL</span>
     <span className={'py-[3px] px-[7px] bg-border-light rounded-[3px]'}>K</span>

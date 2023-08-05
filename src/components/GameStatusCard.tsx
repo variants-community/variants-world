aeb94b1767eaa5484073fb05f60feb57ccbe8919
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-08-05 16:40:47 +0300

    feat: post page status-card is completed

---
+++ src/components/GameStatusCard.tsx
@@ -0,0 +1,121 @@
+import { useEffect, useState } from 'preact/hooks'
+import Bubbles from './icons/Bubbles'
+import { supabase } from '../db/supabase/supabase'
+import { GameStatus } from '@prisma/client'
+import EditIcon from './icons/EditIcon'
+import { getValueFromEvent, statusToColor, statusToString } from '../hepers'
+
+type AcceptedCardProps = {
+  verdict: string | null;
+  postId: number;
+  status: GameStatus;
+};
+
+const statuses = [
+  { label: 'Accepted', value: GameStatus.ACCEPTED },
+  { label: 'Declined', value: GameStatus.DECLINED },
+  { label: 'Pending reply', value: GameStatus.PENDING_REPLY },
+  { label: 'Under review', value: GameStatus.UNDER_REVIEW },
+]
+const GameStatusCard = (props: AcceptedCardProps) => {
+  const [verdict, setVerdict] = useState(props.verdict ?? '')
+  const [status, setStatus] = useState(props.status)
+  const [isChangeable, setIsChangeable] = useState(false)
+
+  const toogleIsChangeable = () => {
+    setIsChangeable(!isChangeable)
+  }
+
+  useEffect(() => {
+    const channel = supabase.channel('adminSettings channel')
+      .on(
+        'postgres_changes',
+        {
+          event: 'UPDATE',
+          schema: 'public',
+          table: 'Post',
+          filter: `id=eq.${props.postId}`,
+        },
+        (payload) => {
+          const updated = payload.new as { verdict: string, status: GameStatus }
+          setVerdict(updated.verdict)
+          setStatus(updated.status)
+          console.log('realtime update: ', updated)
+        },
+      ).subscribe()
+
+    return () => {
+      supabase.removeChannel(channel)
+    }
+  }, [supabase, status, verdict])
+
+  const onStatusChange = async (e: Event) => {
+    const status = getValueFromEvent<GameStatus>(e)
+    await supabase.from('Post').update({ status: status }).eq('id', props.postId)
+  }
+
+  const onVerdictChange = async (e: Event) => {
+    const verdict = getValueFromEvent<string>(e)
+    await supabase.from('Post').update({ verdict: verdict }).eq('id', props.postId)
+  }
+  console.log('statusToColor(status): ', statusToColor(status))
+  return (
+    <div
+      className={`relative flex flex-row justify-between items-center text-center bg-border-light text-${
+        statusToColor(status)
+      } shadow-dark rounded-[12px]`}
+    >
+      <Bubbles
+        className="ml-[48px] mt-[45px] mb-[20px] mr-0"
+        color={statusToColor(status)}
+      />
+      <div className={'block w-full'}>
+        {!isChangeable
+          ? (
+            <h2 className={'text-[40px] font-semibold mt-[16px]'}>
+              {statusToString(status)}
+            </h2>
+          )
+          : (
+            <select
+              value={status}
+              onChange={onStatusChange}
+              type="text"
+              className={'bg-dark rounded-[3px] text-center block text-[40px] font-semibold mt-[16px] mx-auto outline-none'}
+            >
+              {statuses.map((status, i) => (
+                <option key={i} value={status.value}>{status.label}</option>
+              ))}
+            </select>
+          )}
+
+        {!isChangeable
+          ? (
+            <p
+              className={'text-[16px] font-semibold mt-[7px] mb-[20px] text-center'}
+            >
+              {verdict}
+            </p>
+          )
+          : (
+            <input
+              value={verdict}
+              onChange={onVerdictChange}
+              type="text"
+              className={'bg-dark rounded-[3px] text-[16px] font-semibold mt-[7px] mb-[20px] text-center outline-none'}
+            />
+          )}
+      </div>
+      <Bubbles
+        className="mr-[48px] mt-[45px] mb-[20px] miror"
+        color={statusToColor(status)}
+      />
+
+      <div onClick={() => toogleIsChangeable()} className={'cursor-pointer '}>
+        <EditIcon className="absolute bottom-[12px] right-[10px] text-green" />
+      </div>
+    </div>
+  )
+}
+
+export default GameStatusCard
--- src/components/icons/Bubbles.tsx
+++ src/components/icons/Bubbles.tsx
@@ -1,4 +1,4 @@
-const Bubbles = (props: { className?: string }) => (
+const Bubbles = (props: { className?: string, color: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="125"
@@ -7,13 +7,13 @@ const Bubbles = (props: { className?: string }) => (
     fill="none"
     className={`${props.className}`}
   >
-    <rect x="28" y="14" width="8" height="8" rx="4" fill="#61AD14" />
-    <rect width="5" height="5" rx="2.5" fill="#61AD14" />
-    <rect x="96" y="4" width="10" height="10" rx="5" fill="#61AD14" />
-    <rect x="64" y="55" width="8" height="8" rx="4" fill="#61AD14" />
-    <rect x="26" y="61" width="5" height="5" rx="2.5" fill="#61AD14" />
-    <rect x="46" y="82" width="5" height="5" rx="2.5" fill="#61AD14" />
-    <rect x="113" y="102" width="12" height="12" rx="6" fill="#61AD14" />
+    <rect x="28" y="14" width="8" height="8" rx="4" fill={props.color} />
+    <rect width="5" height="5" rx="2.5" fill={props.color} />
+    <rect x="96" y="4" width="10" height="10" rx="5" fill={props.color} />
+    <rect x="64" y="55" width="8" height="8" rx="4" fill={props.color} />
+    <rect x="26" y="61" width="5" height="5" rx="2.5" fill={props.color} />
+    <rect x="46" y="82" width="5" height="5" rx="2.5" fill={props.color} />
+    <rect x="113" y="102" width="12" height="12" rx="6" fill={props.color} />
   </svg>
 )
 

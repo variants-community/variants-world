954b14051e5b3add1ec7717d9ccd84ba4e222b14
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-08-12 17:16:48 +0300

    refactoring: useLikes

---
+++ src/components/Likes/index.tsx
@@ -0,0 +1,33 @@
+import LikeIcon from '../icons/LikeIcon'
+import type { PostOnUserLikes } from '@prisma/client'
+import { useLikes } from './useLikes'
+
+type LikesProps = {
+  likes: PostOnUserLikes[];
+  userId: number;
+  postId: number;
+};
+
+const Likes = (props: LikesProps) => {
+  
+  const { isLiked, likesCount, toogleLike } = useLikes(props.likes, props.userId, props.postId)
+
+  return (
+    <div
+      onClick={() => toogleLike()}
+      aria-hidden="true"
+      className={'flex flex-row justify-end whitespace-nowrap gap-[8px] parent transition-all duration-100'}
+    >
+      <span className={`text-[22px] ${isLiked ? 'text-red' : ''}`}>
+        {likesCount}
+      </span>
+
+      <LikeIcon
+        className="child hover:fill-red transition duration-100"
+        isLiked={isLiked}
+      />
+    </div>
+  )
+}
+
+export default Likes
---
+++ src/components/Likes/useLikes.ts
@@ -0,0 +1,75 @@
+import { useEffect, useState } from 'preact/hooks'
+import { getLikesCountQuery, isPostLikedByUserQuery, putLikeQuery, removeLikeQuery } from '../../db/supabase/queries'
+import { supabase } from '../../db/supabase/supabase'
+import type { PostOnUserLikes } from '@prisma/client'
+
+
+
+export const useLikes = (likes: PostOnUserLikes[], userId: number, postId: number) => {
+  const [isLiked, setIsLiked] = useState<boolean>(
+    likes.find((like) => like.userId === userId) ? true : false
+  )
+  const [likesCount, setLikesCount] = useState<number>(likes.length)
+
+  const updateLiksCount = async () => {
+    const count = await getLikesCountQuery(postId)
+    setLikesCount(count)
+  }
+
+  useEffect(() => {
+    const channel = supabase
+      .channel('likes')
+      .on(
+        'postgres_changes',
+        {
+          event: 'INSERT',
+          schema: 'public',
+          table: 'PostOnUserLikes',
+          filter: `postId=eq.${postId}`
+        },
+        async () => {
+          await updateLiksCount()
+        }
+      )
+      .on(
+        'postgres_changes',
+        {
+          event: 'DELETE',
+          schema: 'public',
+          table: 'PostOnUserLikes',
+          filter: `postId=eq.${postId}`
+        },
+        async () => {
+          await updateLiksCount()
+        }
+      )
+      .subscribe()
+
+    return () => {
+      supabase.removeChannel(channel)
+    }
+  }, [supabase])
+
+  const toogleLike = async () => {
+    // for expirience, we immediately change state
+    // but after will be set correctly
+    setLikesCount(isLiked ? likesCount - 1 : likesCount + 1)
+    setIsLiked(!isLiked)
+
+    if (await isPostLikedByUserQuery(postId, userId)) {
+      const ok = await removeLikeQuery(postId, userId)
+      setIsLiked(ok ? false : true)
+    } else {
+      const ok = await putLikeQuery(postId, userId)
+      setIsLiked(ok)
+    }
+
+    await updateLiksCount()
+  }
+
+  return {
+   isLiked,
+   likesCount,
+   toogleLike,
+  }
+}

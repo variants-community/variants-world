965c5eaf5457b744d098f7390905b2b10de8a370
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-08-16 17:24:27 +0300

    feat: sketch of presence

---
+++ src/components/Presence/index.tsx
@@ -0,0 +1,33 @@
+import type { AuthentificatedUser } from '../../db/supabase/auth'
+import { usePresence } from './usePresence'
+
+type PresenceProps = {
+  postId: number;
+  user: AuthentificatedUser;
+};
+
+const Presence = (props: PresenceProps) => {
+  const { usersOnPost } = usePresence(props.postId, props.user)
+  return (
+    <>
+      {usersOnPost.length > 0 &&
+        (
+          <div className={'flex flex-row items-center text-[24px]'}>
+            <span className={'mr-[10px]'}>Here now:</span>
+            <div className={'flex flex-wrap gap-[10px] font-semibold'}>
+              {usersOnPost.map((u) => (
+                <span
+                  className={'px-[5px] py-[2px] bg-gray rounded'}
+                  key={u.id}
+                >
+                  {u.username}
+                </span>
+              ))}
+            </div>
+          </div>
+        )}
+    </>
+  )
+}
+
+export default Presence
---
+++ src/components/Presence/usePresence.ts
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'preact/hooks'
+import type { AuthentificatedUser } from '../../db/supabase/auth'
+import { supabase } from '../../db/supabase/supabase'
+import type { RealtimePresenceState } from '@supabase/supabase-js'
+
+const mapPresenceState = (state: RealtimePresenceState) => {
+  return Object.keys(state)
+    .map((presenceId) => {
+      const presences = state[presenceId] as unknown as {
+        user: AuthentificatedUser
+      }[]
+
+      return presences.map((presence) => presence.user)
+    })
+    .flat()
+}
+
+export const usePresence = (postId: number, user: AuthentificatedUser) => {
+  const [usersOnPost, setUsersOnPost] = useState<AuthentificatedUser[]>([])
+
+  useEffect(() => {
+    const channel = supabase
+      .channel(`room-${postId}`, {
+        config: {
+          presence: { key: user.id.toString() }
+        }
+      })
+      .on('presence', { event: 'sync' }, () => {
+        const presenceState = channel.presenceState()
+        const users = mapPresenceState(presenceState)
+        setUsersOnPost(users)
+      })
+      .subscribe(async (status) => {
+        if (status === 'SUBSCRIBED') {
+          await channel.track({
+            user: user,
+            online_at: new Date().toISOString()
+          })
+        }
+      })
+
+    return () => {
+      channel.unsubscribe()
+    }
+  }, [user.id])
+
+  return {
+    usersOnPost: [
+      ...new Map(usersOnPost.map((u) => [u.id, u])).values()
+    ].filter((u) => u.id !== user.id)
+  }
+}

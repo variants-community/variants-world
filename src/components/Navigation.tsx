4c3257eb84f95a68314304c9a77462bebc6a7ae4
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-08-26 01:38:31 +0300

    refactor: new variant button styles

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,25 +1,21 @@
-const buttons = [
-  { label: 'Add variant', link: '/posts/new' },
-]
-
 type NavigationProps = {
   username: string;
 };
 
 const Navigation = (props: NavigationProps) => {
   return (
-    <nav className={'fixed sm:static sm:mb-[30px] flex flex-row justify-between items-center bg-border-light h-[50px] w-full px-[20px] sm:rounded-[12px] sm:border sm:border-[2px] sm:border-border-dark shadow-dark'}>
-      <div className={'flex flex-row gap-[30px]'}>
+    <nav
+      className={'fixed sm:static sm:mb-[30px] flex flex-row justify-between items-center bg-border-light h-[50px] w-full px-[20px] sm:rounded-[12px] sm:border sm:border-[2px] sm:border-border-dark shadow-dark'}
+    >
+      <div className={'flex flex-row gap-[20px] sm:gap-[40px]'}>
         <a href={'/'} className={'text-white  text-[28px]'}>Variants</a>
-        {buttons.map((botton, i) => (
-          <a
-            key={i}
-            href={botton.link}
-            className={'bg-gray rounded-[3px] px-[5px] py-[2px]'}
-          >
-            {botton.label}
-          </a>
-        ))}
+
+        <a
+          href={'/posts/new'}
+          className={'flex bg-opacity-40 items-center hover:(text-white bg-opacity-35) h-[30px] bg-gray text-[18px] align-middle rounded-[6px] px-[7px] transition-colors duration-300 ease-out'}
+        >
+          Add your variant!
+        </a>
       </div>
       <div className={'flex flex-row gap-[10px]'}>
         {props.username}

1909f132ea2e852ceea78b7ae268c2c7b74ab0f7
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-08-29 20:13:33 +0300

    fix: likes. temporary solution

--- src/components/Likes/use-likes.ts
+++ src/components/Likes/use-likes.ts
@@ -1,11 +1,10 @@
-import { getLikesCountQuery, isPostLikedByUserQuery, putLikeQuery, removeLikeQuery } from 'db/supabase/queries'
+import { getLikesCountQuery, putLikeQuery, removeLikeQuery } from 'db/supabase/queries'
 import { supabase } from 'db/supabase/supabase'
 import { useEffect, useState } from 'preact/hooks'
 
 export const useLikes = (likes: { userId: number }[], userId: number, postId: number) => {
   const [isLiked, setIsLiked] = useState<boolean>(likes.some(like => like.userId === userId))
   const [likesCount, setLikesCount] = useState<number>(likes.length)
-  const [likeTimeout, setLikeTimeout] = useState<number>()
 
   const updateLiksCount = async () => {
     const count = await getLikesCountQuery(postId)
@@ -52,19 +51,12 @@ export const useLikes = (likes: { userId: number }[], userId: number, postId: nu
     setLikesCount(isLiked ? likesCount - 1 : likesCount + 1)
     setIsLiked(!isLiked)
 
-    if (likeTimeout) clearTimeout(likeTimeout)
-    else {
-      setLikeTimeout(
-        window.setTimeout(async () => {
-          if (await isPostLikedByUserQuery(postId, userId)) {
-            const ok = await removeLikeQuery(postId, userId)
-            setIsLiked(ok ? false : true)
-          } else {
-            const ok = await putLikeQuery(postId, userId)
-            setIsLiked(ok)
-          }
-        }, 500)
-      )
+    if (isLiked) {
+      const ok = await removeLikeQuery(postId, userId)
+      setIsLiked(ok ? false : true)
+    } else {
+      const ok = await putLikeQuery(postId, userId)
+      setIsLiked(ok)
     }
   }
 
@@ -74,3 +66,10 @@ export const useLikes = (likes: { userId: number }[], userId: number, postId: nu
     toogleLike
   }
 }
+
+// isLiked
+//
+//
+//
+//
+//

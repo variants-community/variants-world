3df7996011b497b5a188f4448b66ce91a5d408ca
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-08-29 17:18:06 +0300

    refactor: remove game rules edit mode (unnecessary)

--- src/components/PostTags/index.tsx
+++ src/components/PostTags/index.tsx
@@ -10,29 +10,9 @@ type PostTagsProps = {
 }
 
 const PostTags = (props: PostTagsProps) => {
-  // const { rules, edit, remove, add } = usePostTagsEditMode(
-  //   props.rules,
-  //   props.postId,
-  // )
-
-  // const withEditingMode = !!props.postId // if the postId is not passed - there is no possibility to edit
-
   return (
     <div>
       <ul class={`flex flex-wrap items-center list-none gap-[10px] text-[14px] font-bold ${props.ulclass}`}>
-        {/* {withEditingMode && rules.sort().map((rule) => (
-          <RuleItemEditable
-            key={rule}
-            ruleName={rule}
-            isEditMode={props.isEditMode ?? false}
-            class={props.class}
-            onRemove={() => remove?.(rule)}
-            edit={edit}
-          />
-        ))}
-        {withEditingMode && props.isEditMode && (
-          <AddRuleButton addRule={add} class={props.class} />
-        )} */}
         {props.rules.sort().map(rule => (
           <RuleItem key={rule} ruleName={rule} class={props.class} />
         ))}

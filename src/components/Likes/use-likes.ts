a3a5f570fcbaa3cc6eed7dae1aae1367937e0df0
Levkov Gleb <levkov.gleb.dev@gmail.com>
2023-09-03 22:51:27 +0300

    refactor: useLikes (use signals)

--- src/components/Likes/use-likes.ts
+++ src/components/Likes/use-likes.ts
@@ -1,14 +1,15 @@
 import { getLikesCountQuery, putLikeQuery, removeLikeQuery } from 'db/supabase/queries'
 import { supabase } from 'db/supabase/supabase'
-import { useEffect, useState } from 'preact/hooks'
+import { useEffect } from 'preact/hooks'
+import { useSignal } from '@preact/signals'
 
 export const useLikes = (likes: { userId: number }[], userId: number, postId: number) => {
-  const [isLiked, setIsLiked] = useState<boolean>(likes.some(like => like.userId === userId))
-  const [likesCount, setLikesCount] = useState<number>(likes.length)
+  const isLiked = useSignal(likes.some(like => like.userId === userId))
+  const likesCount = useSignal(likes.length)
 
   const updateLiksCount = async () => {
     const count = await getLikesCountQuery(postId)
-    setLikesCount(count)
+    likesCount.value = count
   }
 
   useEffect(() => {
@@ -48,21 +49,21 @@ export const useLikes = (likes: { userId: number }[], userId: number, postId: nu
   const toogleLike = async () => {
     // for expirience, we immediately change state
     // but after will be set correctly
-    setLikesCount(isLiked ? likesCount - 1 : likesCount + 1)
-    setIsLiked(!isLiked)
+    likesCount.value = isLiked.value ? likesCount.value - 1 : likesCount.value + 1
+    isLiked.value = !isLiked.value
 
-    if (isLiked) {
+    if (!isLiked.value) {
       const ok = await removeLikeQuery(postId, userId)
-      setIsLiked(ok ? false : true)
+      isLiked.value = ok ? false : true
     } else {
       const ok = await putLikeQuery(postId, userId)
-      setIsLiked(ok)
+      isLiked.value = ok
     }
   }
 
   return {
-    isLiked,
-    likesCount,
+    isLiked: isLiked.value,
+    likesCount: likesCount.value,
     toogleLike
   }
 }

7d59b8ce72a1201888e4bbfcc9b4106cf053e7db
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2023-10-07 18:54:57 +0300

    build: update types generation scripts

--- src/seeds/db-types.ts
+++
@@ -1,8 +0,0 @@
-import { exec } from 'child_process'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const command = `npx supabase gen types typescript --project-id "${process.env.SUPABASE_PROJECT_ID}" --schema public > ./src/db/supabase/types.ts`
-
-exec(command, err => (err ? console.error('could not execute command: ', err) : console.log('successfully')))

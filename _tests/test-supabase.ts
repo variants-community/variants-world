8f4e46577d2c858d6da1b4e39e5294fdcc87b09a
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2023-11-28 00:12:01 +0300

    ci: init rls tests

---
+++ _tests/test-supabase.ts
@@ -0,0 +1,12 @@
+import { createClient } from '@supabase/supabase-js'
+import type { Database } from 'db/supabase/types'
+
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
+
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false
+  }
+})

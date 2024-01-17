1db2cfea148e43c16c98c4c8e4eb3629390f6f41
Levkov Gleb <levkov.gleb.dev@gmail.com>
2024-01-18 02:00:29 +0300

    feat: partial display of huge rule tags | tooltip

--- src/components/PostTags/RuleItem.tsx
+++ src/components/PostTags/RuleItem.tsx
@@ -3,12 +3,23 @@ type RuleItemProps = {
   class?: string
 }
 
+const RULE_ITEM_MAX_SIZE = 40
+
+const minimize = (ruleName: string) => {
+  return ruleName.length > RULE_ITEM_MAX_SIZE ? `${ruleName.substring(0, RULE_ITEM_MAX_SIZE)}...` : ruleName
+}
+
 export const RuleItem = (props: RuleItemProps) => {
   return (
     <li
       class={`h-5 sm:h-7 flex flex-row items-center gap-[5px] whitespace-nowrap bg-dark border border-border-dark rounded px-[5px] ${props.class}`}
     >
-      <span>{props.ruleName}</span>
+      <span
+        data-tooltip={props.ruleName.length > RULE_ITEM_MAX_SIZE ? props.ruleName : undefined}
+        data-tooltip-position="bottom"
+      >
+        {minimize(props.ruleName)}
+      </span>
     </li>
   )
 }

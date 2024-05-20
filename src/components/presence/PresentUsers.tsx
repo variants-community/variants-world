327739c70cce09bc25cd7b494e92c77d70c8c9f1
Levkov Gleb <levkov.gleb.dev@gmail.com>
2024-05-20 17:08:38 +0300

    feat: add username popup | refactor users present

--- src/components/presence/PresentUsers.tsx
+++ src/components/presence/PresentUsers.tsx
@@ -11,14 +11,27 @@ const PresentUsers = (props: PresenceProps) => {
   return (
     <div class={'flex flex-row-reverse w-full py-3'}>
       {usersOnPost.map((user, i) => (
-        <img
-          key={`${user.id}${i}`}
-          class={`h-8 w-8 rounded-full ring-primary ring-3 shadow-dark
-          ${i ? 'mr-[-5px]' : ''}`}
-          src={props.user.profileUrl ?? '/assets/images/user.png'}
-          title={user.username}
-        />
+        <div class={`group relative h-8 w-8  ${i ? 'mr-[-5px]' : ''}`}>
+          <img
+            key={`${user.id}${i}`}
+            class={` h-8 w-8 rounded-full ring-primary ring-3 shadow-dark`}
+            src={user.profileUrl ?? '/assets/images/user.png'}
+          />
+          <PresentUsersPopup username={user.username} />
+        </div>
       ))}
+    </div>
+  )
+}
+
+type PresencePopupProps = {
+  username: string
+}
+
+const PresentUsersPopup = ({ username }: PresencePopupProps) => {
+  return (
+    <div class="absolute bottom-[-40px]  px-2 py-1 rounded-md shadow bg-border-light border-1 border-border-dark opacity-0 group-hover:opacity-[100%] transition-all duration-100 easy-in z-1">
+      <span>{username}</span>
     </div>
   )
 }

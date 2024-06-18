9d2bed224b01dffcc115273de25153e43f2d9132
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2024-06-18 20:34:34 +0300

    fix: use prod-env for dynamic import (#37)

--- src/db/prisma/prisma.js
+++ src/db/prisma/prisma.js
@@ -1,6 +1,6 @@
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-const def = await (import.meta.env.VW_ENV === 'prod' ? import('prisma/generated/deno') : import('@prisma/client'))
+const def = await (import.meta.env.PROD ? import('prisma/generated/deno') : import('@prisma/client'))
 const prisma = new def.PrismaClient().$extends(withAccelerate())
 
 export default def.Prisma

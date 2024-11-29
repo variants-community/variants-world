5d247888cf130f69cab60d1d748900e54c9ca046
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2024-11-30 01:22:15 +0300

    build: add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@ import { defineConfig, passthroughImageService } from 'astro/config'
 import preact from '@astrojs/preact'
 import windi from 'astro-windi'
 
-import deno from '@deno/astro-adapter'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), windi()],
   output: 'server',
   server: { port: 3000 },
-  adapter: deno(),
+  adapter: netlify({ edgeMiddleware: true }),
   image: {
     service: passthroughImageService()
   }

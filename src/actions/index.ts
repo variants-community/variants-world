ee345da40585bdaf0bfe67ee43af8a2acc3ee333
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2025-02-01 21:39:11 +0300

    feat: update navbar

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,9 +2,11 @@ import { createPost } from 'actions/create-post'
 import { getFilteredPosts } from 'actions/get-filtered-posts'
 import { getGameDetails } from 'actions/get-game-details'
 import { getPosts } from 'actions/get-posts'
+import { starsWebhook } from 'actions/stars-webhook'
 import { validateGames } from 'actions/validate-games'
 
 export const server = {
+  starsWebhook,
   getPosts,
   getFilteredPosts,
   createPost,

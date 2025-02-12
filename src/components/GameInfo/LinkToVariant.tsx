b7bc4b160005dbb0541fc583c48c4fdc822c0341
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2025-02-12 23:09:35 +0300

    feat: include all testing games to post

--- src/components/GameInfo/LinkToVariant.tsx
+++ src/components/GameInfo/LinkToVariant.tsx
@@ -1,7 +1,9 @@
-export const LinkToVariant = ({ value }: { value: string }) => (
+import type { JSX } from 'preact'
+
+export const LinkToVariant = ({ value, children }: { value: string; children?: JSX.Element | string | number }) => (
   <>
     <a href={value} class={'h-7 flex items-center bg-primary px-8 rounded text-white font-semibold text-[14px]'}>
-      Try this variant
+      {children}
     </a>
   </>
 )

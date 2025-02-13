852562138811e96ad52912ed1e4a275eacf4662d
Artur Sharapov <38008115+ArturSharapov@users.noreply.github.com>
2025-02-14 02:12:19 +0300

    feat: add tooltip with exact date for posts

--- src/components/GameInfo/TimePassed.tsx
+++ src/components/GameInfo/TimePassed.tsx
@@ -1,7 +1,21 @@
 import { getFormattedTimePassed } from 'utils/hepers'
 
 export const TimePassed = ({ from }: { from: Date }) => (
-  <div class={'items-center'}>
+  <div
+    class={
+      'items-center before:(pointer-events-none opacity-0 whitespace-nowrap !duration-0 transition-opacity !block) after:(pointer-events-none opacity-0 whitespace-nowrap !duration-0 transition-opacity !block) hover:before:(opacity-100 delay-300) hover:after:(opacity-100 delay-300)'
+    }
+    data-tooltip={new Date().toLocaleString('US-en', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    })}
+    data-tooltip-position="bottom"
+  >
     <span class={'whitespace-nowrap'}>{`${getFormattedTimePassed(new Date(from))}`}</span>
   </div>
 )
